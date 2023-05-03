66d4770737a99bc35d707fdb79669250f3c4bd44
Kixen93 <43902662+Kixen93@users.noreply.github.com>
2023-05-03 19:41:21 +0200

    gyakorlás

---
+++ src/gyak230503/Gyak230503.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gyak230503;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author itsso
+ */
+public class Gyak230503 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    enum PartOfDay{
+    MORNING, NOON, AFTERNOON, NIGHT;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*Integer i1 = 1;
+        int i2=1;
+        Integer i3 = 1;
+        Integer i4 = new Integer(1);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+        System.out.println(i1 == i3);
+        System.out.println(i1 == i4.intValue());*/
+        
+        
+        Date d = Calendar.getInstance().getTime();
+        System.out.println(d);
+        int hour = d.getHours();
+        System.out.println(hour);
+        PartOfDay n;
+        if (hour < 12)
+        n= PartOfDay.MORNING;
+        else if (hour < 13)
+        n= PartOfDay.NOON;
+        else if (hour < 19)
+        n= PartOfDay.AFTERNOON;
+        else
+        n= PartOfDay.NIGHT;
+        System.out.println("Napszak: " + n);
+        
+        
+        
+    }
+    
+}
